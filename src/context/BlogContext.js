2516b8792abe1aeb91e1b6a9010c02ac7829db2d
Elliot Kim <elliotkim916@gmail.com>
2020-04-02 17:02:11 -0700

    Added edit features

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,10 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch(action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
@@ -15,7 +19,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: 'add_blogpost', payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -25,7 +31,16 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'Test Post', content: 'Test Content', id: 1 }]);
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content }});
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'Test Post', content: 'Test Content', id: 1 }]);
 
 // Provider is within the BlogContext object
 // Provider is whats going to accept some information, 
